85cb3f143b7089720ca4c2ba6f6df26c1ff80828
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-01 01:08:52 +0200

    feat: Improve users and clients fragment

--- app/src/main/java/com/example/hueverianieto/ui/UsersAndClientsFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/UsersAndClientsFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.example.hueverianieto.R
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.databinding.FragmentUsersAndClientsBinding
 
 class UsersAndClientsFragment: BaseFragment() {
 
     private lateinit var binding: FragmentUsersAndClientsBinding
+    private lateinit var view: View
 
     override fun injection() {
         // TODO: sin implementar
@@ -40,6 +41,7 @@ class UsersAndClientsFragment: BaseFragment() {
     ): View {
         this.binding = FragmentUsersAndClientsBinding
             .inflate(inflater, container, false)
+        this.view = inflater.inflate(R.layout.fragment_users_and_clients, container, false)
         return this.binding.root
     }
 
