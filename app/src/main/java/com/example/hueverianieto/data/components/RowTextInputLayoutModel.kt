0ef2bff3bf80750c02b3651c8ffc6667ab925dbf
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-16 21:07:10 +0200

    feat: Add component row textInput for recyclerview

---
+++ app/src/main/java/com/example/hueverianieto/data/components/RowTextInputLayoutModel.kt
@@ -0,0 +1,6 @@
+package com.example.hueverianieto.data.components
+
+class RowTextInputLayoutModel(
+    var leftTextInputLayoutModel: TextInputLayoutModel,
+    var rightTextInputLayoutModel: TextInputLayoutModel? = null
+)
---
+++ app/src/main/java/com/example/hueverianieto/data/components/TextInputLayoutModel.kt
@@ -0,0 +1,14 @@
+package com.example.hueverianieto.data.components
+
+import android.text.method.TransformationMethod
+
+class TextInputLayoutModel(
+    var inputText: String? = null,
+    var inputType: Int? = null,
+    var hintText: String? = null,
+    var radius: Float? = null,
+    var strokeColor: Int? = null,
+    var iconMode: Int? = null,
+    var transformationMethod: TransformationMethod? = null,
+    var maxLines: Int? = null
+)
