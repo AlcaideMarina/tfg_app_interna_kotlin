a2b913ad1ddad73c2457f16fda08fe047b745b4e
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-19 00:22:31 +0200

    feat: Create AllInternalUsersActivity

---
+++ app/src/main/java/com/example/hueverianieto/ui/usersandclients/users/AllInternalUsersActivity.kt
@@ -0,0 +1,49 @@
+package com.example.hueverianieto.ui.usersandclients.users
+
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hueverianieto.R
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.databinding.ActivityAllInternalUsersBinding
+
+class AllInternalUsersActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityAllInternalUsersBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun injection() {
+        // TODO: Sin implementar
+    }
+
+    override fun setUp() {
+        this.binding = ActivityAllInternalUsersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(this.binding.topBar)
+        navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        this.binding.topBar.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.allInternalUsersFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        configNav("Ver usuarios internos")
+    }
+
+    override fun configureUI() {
+        // TODO: Sin implementar
+    }
+
+    override fun setListeners() {
+        // TODO: Sin implementar
+    }
+
+    fun configNav(title: String) {
+        this.binding.topBar.title = title
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        this.binding.topBar.setNavigationOnClickListener { this.onBackPressedDispatcher.onBackPressed() }
+    }
+
+}
