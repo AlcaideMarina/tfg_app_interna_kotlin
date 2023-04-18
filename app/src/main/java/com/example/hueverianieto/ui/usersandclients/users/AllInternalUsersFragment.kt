495f3b488065a4d85495b126ff6a20a0954657ed
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-04-19 01:46:56 +0200

    feat: Create bbdd request to get interanl users lista

--- app/src/main/java/com/example/hueverianieto/ui/usersandclients/users/AllInternalUsersFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/usersandclients/users/AllInternalUsersFragment.kt
@@ -1,13 +1,22 @@
 package com.example.hueverianieto.ui.usersandclients.users
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.components.componentinternaluseradapter.ComponentInternalUserAdapter
 import com.example.hueverianieto.data.components.ComponentInternalUserModel
 import com.example.hueverianieto.databinding.FragmentAllInternalUsersBinding
+import com.example.hueverianieto.ui.usersandclients.UsersAndClientsFragment
 import com.example.hueverianieto.ui.usersandclients.clients.AllClientsActivity
+import com.example.hueverianieto.utils.ClientUtils
+import com.example.hueverianieto.utils.UserUtils
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AllInternalUsersFragment : BaseFragment() {
 
@@ -24,6 +33,7 @@ class AllInternalUsersFragment : BaseFragment() {
         this.binding.newInternalUserButton.setText("Nuevo")
         this.binding.deletedInternalUsersButton.isEnabled = true
         this.binding.deletedInternalUsersButton.setText("Usuarios eliminados")
+        getInternalUserListaData()
     }
 
     override fun setObservers() {
@@ -43,6 +53,72 @@ class AllInternalUsersFragment : BaseFragment() {
         this.binding = FragmentAllInternalUsersBinding
             .inflate(inflater, container, false)
         return this.binding.root
+    }
+
+    private fun getInternalUserListaData() {
+        Firebase.firestore
+            .collection("user_info")
+            .orderBy("id")
+            .get()
+            .addOnCompleteListener { task ->
+                internalUserList = mutableListOf()
+                if (task.isSuccessful) {
+                    val documentList = task.result
+                    if (!documentList.isEmpty) {
+                        this.binding.internalUsersRecyclerView.visibility = View.VISIBLE
+                        this.binding.containerWaringNoInternalUsers.visibility = View.GONE
+                        for (document in documentList) {
+                            Log.v("CONSULTA", UsersAndClientsFragment::class.java.simpleName + " - Consulta correcta")
+                            val doc = document.data as MutableMap<String, Any?>?
+                            if (UserUtils.checkErrorMap(doc) == null) {
+                                val data = doc as MutableMap<String, Any?>
+                                val userData = UserUtils.mapToParcelable(data, document.id)
+                                if (!userData.deleted) {
+                                    val componentInternalUserModel = ComponentInternalUserModel(
+                                        userData.id,
+                                        userData.name,
+                                        userData.surname,
+                                        userData.dni,
+                                        "userData.role"
+                                    ) {
+                                        // TODO
+                                        Log.v("NAVEGACIÓN", AllInternalUsersFragment::class.java.simpleName)
+                                    }
+                                    internalUserList.add(componentInternalUserModel)
+                                }
+                            } else {
+                                // TODO
+                                Log.e("CONSULTA", AllInternalUsersFragment::class.java.simpleName + " hay un error en el objeto")
+                            }
+                        }
+                        if (internalUserList.isEmpty()) {
+                            this.binding.internalUsersRecyclerView.visibility = View.GONE
+                            this.binding.containerWaringNoInternalUsers.visibility = View.VISIBLE
+                            this.binding.containerWaringNoInternalUsers.setTitle("No hay usuarios internos")
+                            this.binding.containerWaringNoInternalUsers.setText("No hay registro de usuarios internos en la base de datos")
+                        } else {
+                            initRecyclerView()
+                        }
+                    } else {
+                        this.binding.internalUsersRecyclerView.visibility = View.GONE
+                        this.binding.containerWaringNoInternalUsers.visibility = View.VISIBLE
+                        this.binding.containerWaringNoInternalUsers.setTitle("No hay clientes")
+                        this.binding.containerWaringNoInternalUsers.setText("No hay registro de clientes activos en la base de datos")
+                    }
+                } else {
+                    // TODO
+                    Log.e("CONSULTA", AllInternalUsersFragment::class.java.simpleName + " task.isSuccessful == false - " + task.exception?.message)
+                }
+            }.addOnFailureListener {
+                // TODO
+                Log.e("CONSULTA", AllInternalUsersFragment::class.java.simpleName + " fallo en la llamada")
+            }
+    }
+
+    private fun initRecyclerView() {
+        this.binding.internalUsersRecyclerView.layoutManager = LinearLayoutManager(context)
+        this.binding.internalUsersRecyclerView.adapter = ComponentInternalUserAdapter(internalUserList)
+        this.binding.internalUsersRecyclerView.setHasFixedSize(false)
     }
 
 }
