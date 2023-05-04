6bebd38301bfc7676976c18760d25ab5e6496880
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-04 16:56:38 +0200

    refactor: Clean clientdetailfragment

--- app/src/main/java/com/example/hueverianieto/ui/views/clients/fragments/clientdetail/ClientDetailFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/clients/fragments/clientdetail/ClientDetailFragment.kt
@@ -34,50 +34,6 @@ class ClientDetailFragment : BaseFragment() {
     private lateinit var currentUserData: InternalUserData
     private val clientDetailViewModel : ClientDetailViewModel by viewModels()
 
-
-    private var company : String = ""
-    private var direction : String = ""
-    private var city : String = ""
-    private var province : String = ""
-    private var postalCode : String = ""
-    private var cif : String  = ""
-    private var email : String = ""
-    private var phone1 : String = ""
-    private  var namePhone1 : String = ""
-    private var phone2 : String = ""
-    private var namePhone2 : String = ""
-    private var hasAccount : Boolean = false
-    private var accountEmail : String? = ""
-    private var accountUser : String? = ""
-
-
-    override fun configureUI() {
-
-        setButttons()
-        // TODO: Habrá que controlar la parte de pedidos -> ordersLinearLayout.visibility = View.VISIBLE -> por ahora lo dejo seteado para que se muestre siempre
-        setFieldTexts()
-        setUserInfo()
-
-    }
-
-    override fun setObservers() {
-        // TODO
-    }
-
-    override fun setListeners() {
-        this.binding.checkedTextView.setOnClickListener {
-            this.binding.checkedTextView.isChecked = !this.binding.checkedTextView.isChecked
-            this.binding.userAccountTextInputLayout.isEnabled = this.binding.checkedTextView.isChecked
-            this.binding.emailAccountTextInputLayout.isEnabled = this.binding.checkedTextView.isChecked
-        }
-        this.binding.deleteClientButton.setOnClickListener {  }
-        this.binding.modifyButton.setOnClickListener {  }
-    }
-
-    override fun updateUI(state: BaseState) {
-        //TODO("Not yet implemented")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -98,88 +54,28 @@ class ClientDetailFragment : BaseFragment() {
         return this.binding.root
     }
 
-    private fun setPopUp(title: String, message: String, listener: View.OnClickListener) {
+    override fun configureUI() {
 
-        // TODO: Close button
-        alertDialog.show(
-            requireContext(),
-            ModalDialogModel(
-                title,
-                message,
-                "De acuerdo",
-                null,
-                listener,
-                null,
-                true
-            )
-        )
+        setButtons()
+        // TODO: Habrá que controlar la parte de pedidos -> ordersLinearLayout.visibility = View.VISIBLE -> por ahora lo dejo seteado para que se muestre siempre
+        setFieldTexts()
+        setUserInfo()
+
     }
 
-    private fun updateUser(): String {
-
-        if (clientData.documentId != null) {
-            val userData = ClientData(
-                cif,
-                city,
-                this.clientData.createdBy,
-                company,
-                this.clientData.deleted,
-                direction,
-                email,
-                accountEmail,
-                hasAccount,
-                this.clientData.id,
-                listOf(
-                    mapOf(namePhone1 to phone1.toLong()),
-                    mapOf(namePhone2 to phone2.toLong())
-                ),
-                postalCode.toLong(),
-                province,
-                this.clientData.uid,
-                accountUser,
-                this.clientData.documentId
-            )
-            Firebase.firestore
-                .collection("client_info")
-                .document(this.clientData.documentId!!)
-                .set(ClientUtils.parcelableToMap(userData))
-                .addOnSuccessListener {
-                    Log.d(
-                        NewClientFragment::class.java.simpleName,
-                        "DocumentSnapshot successfully written!"
-                    )
-                    setPopUp(
-                        "Guardado correcto.",
-                        "El cliente ha sido guardado correctamente en la base de datos."
-                    ) {
-                        alertDialog.cancel()
-                        activity?.onBackPressedDispatcher?.onBackPressed()
-                    }
-                }
-                .addOnFailureListener { e ->
-                    Log.w(NewClientFragment::class.java.simpleName, "Error writing document", e)
-                    setPopUp(
-                        "Ha ocurrido un error.",
-                        "Ha ocurrido un error en el poceso de creación del cliente en nuestra base de datos. Por favor, inténtelo de nuevo.\nError: ${e.message}"
-                    ) {
-                        alertDialog.cancel()
-                    }
-                }
-        } else {
-            setPopUp(
-                "Error",
-                "Ha habido un problema al guardar los cambios. Por favor, inténtelo de nuevo más tarde.",
-            ) {
-                alertDialog.cancel()
-            }
-        }
-
-
-
-        return "0000000000"
+    override fun setObservers() {
+        // Not necessary
     }
 
-    private fun setButttons() {
+    override fun setListeners() {
+        // TODO: Listeners de los botones - El resto es estático
+    }
+
+    override fun updateUI(state: BaseState) {
+        // Not necessary
+    }
+
+    private fun setButtons() {
         with(this.binding) {
             modifyButton.setText("Modificar")
             deleteClientButton.setText("Eliminar cliente")
