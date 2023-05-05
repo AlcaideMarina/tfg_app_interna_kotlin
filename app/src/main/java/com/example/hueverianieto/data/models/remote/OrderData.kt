295de5f2ca6e6637992b465a87a7ac7d4af75198
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-06 00:17:00 +0200

    feat: Create ordermodeldata

---
+++ app/src/main/java/com/example/hueverianieto/data/models/remote/OrderData.kt
@@ -0,0 +1,23 @@
+package com.example.hueverianieto.data.models.remote
+
+import android.os.Parcelable
+import com.google.firebase.Timestamp
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class OrderData (
+    val approxDeliveryDatetime: Timestamp,
+    val createdBy: String,
+    val deliveryDatetime: Timestamp?,
+    val deliveryDni: String?,
+    val deliveryNote: Long?,
+    val deliveryPerson: String?,
+    val notes: String?,
+    var order: Map<String, Map<String, Number?>>,
+    val orderDatetime: Timestamp,
+    var orderId: Long?,
+    val paid: Boolean,
+    val paymentMethod: Long,
+    val status: Long,
+    val totalPrice: Number?
+) : Parcelable
