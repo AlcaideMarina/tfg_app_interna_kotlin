f8483086072da442911e7c05939c83083bcb1795
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-06 02:18:21 +0200

    feat: Create adapter and viewholder for RV

---
+++ app/src/main/java/com/example/hueverianieto/ui/components/componentordercontainer/HNOrderContainerViewHolder.kt
@@ -0,0 +1,43 @@
+package com.example.hueverianieto.ui.components.componentordercontainer
+
+import android.icu.text.SimpleDateFormat
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hueverianieto.R
+import com.example.hueverianieto.data.models.local.OrderContainerModel
+import com.example.hueverianieto.databinding.ComponentOrderContainerBinding
+import com.example.hueverianieto.utils.Constants
+import com.example.hueverianieto.utils.Utils
+
+class HNOrderContainerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val binding : ComponentOrderContainerBinding = ComponentOrderContainerBinding.bind(view)
+
+    fun render(orderContainerModel: OrderContainerModel) {
+
+        val sdf = SimpleDateFormat("MM/dd/yyyy")
+        val dateStr = sdf.format(orderContainerModel.orderDate.toDate())
+
+        //TODO: Esto no funciona
+        val priceStr = if (orderContainerModel.price == (-1).toLong()) {
+            "-"
+        } else {
+            orderContainerModel.price.toString()
+        }
+        // TODO: Transformar status
+        var statusStr = Utils.getKey(Constants.orderStatus, orderContainerModel.status.toInt())
+        if (statusStr == null) statusStr = R.string.unknown_status
+
+        this.binding.dateText.text = dateStr
+        this.binding.orderIdText.text = "Pedido: " + orderContainerModel.orderId.toString()
+        this.binding.companyText.text = orderContainerModel.company
+        this.binding.orderSummaryText.text = orderContainerModel.orderSummary
+        this.binding.priceText.text = "Precio: " + priceStr + " €"
+        this.binding.statusDateText.text = this.binding.root.context.getString(statusStr)
+
+        this.binding.baseComponentOrderContainer.setOnClickListener(
+            orderContainerModel.onClickListener )
+
+    }
+
+}
