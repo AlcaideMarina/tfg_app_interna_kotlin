9d5a90d9b74d2e84a053ca9333daf406b46012b8
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-06 02:19:48 +0200

    feat: Set observers

--- app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/orderanddelivery/OrderAndDeliveryFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/orderanddelivery/OrderAndDeliveryFragment.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hueverianieto.R
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.local.OrderContainerModel
 import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentOrdersAndDeliveryBinding
 import com.example.hueverianieto.ui.components.HNModalDialog
+import com.example.hueverianieto.ui.components.componentordercontainer.HNOrderContainerAdapter
 import com.example.hueverianieto.ui.views.main.MainActivity
+import com.example.hueverianieto.utils.OrderUtils
 import com.example.hueverianieto.utils.Utils
 import com.google.firebase.Timestamp
 import dagger.hilt.android.AndroidEntryPoint
@@ -44,7 +49,37 @@ class OrderAndDeliveryFragment : BaseFragment() {
     }
 
     override fun setObservers() {
-        //TODO("Not yet implemented")
+        this.orderAndDeliveryViewModel.allOrderList.observe(this) { orderDataList ->
+            if (orderDataList == null) {
+                // TODO: error
+            } else {
+
+                val orderList = mutableListOf<OrderContainerModel>()
+                for (orderData in orderDataList) {
+                    if (orderData != null) {
+                        val orderContainerModel = OrderContainerModel(
+                            orderData.orderDatetime,
+                            orderData.orderId!!,
+                            orderData.clientId.toString() + " - " + orderData.company,
+                            OrderUtils.getOrderSummary(OrderUtils.orderDataToBDOrderModel(orderData)),
+                            orderData.totalPrice ?: -1,
+                            orderData.status,
+                            orderData.deliveryDni
+                        ) {
+                            // TODO
+                        }
+                        orderList.add(orderContainerModel)
+                    }
+                }
+                if (orderList.isEmpty()) {
+                    // TODO: Vacío
+                } else {
+                    this.binding.todayOrderRecyclerView.layoutManager = LinearLayoutManager(context)
+                    this.binding.todayOrderRecyclerView.adapter = HNOrderContainerAdapter(orderList)
+                    this.binding.todayOrderRecyclerView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     override fun setListeners() {
