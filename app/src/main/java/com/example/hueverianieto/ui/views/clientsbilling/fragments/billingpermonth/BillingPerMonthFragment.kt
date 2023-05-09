e015f7f074a12d468b051e4d5e561e963b4c5e48
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-09 20:48:48 +0200

    feat: Create billing per month frg with MVVM arq
    + add in nav_graph

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/clientsbilling/fragments/billingpermonth/BillingPerMonthFragment.kt
@@ -0,0 +1,119 @@
+package com.example.hueverianieto.ui.views.clientsbilling.fragments.billingpermonth
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.remote.ClientData
+import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.databinding.FragmentBillingPerMonthBinding
+import com.example.hueverianieto.domain.model.billingcontaineritemmodel.BillingContainerItemModel
+import com.example.hueverianieto.ui.components.componentbillingpermonth.ComponentBillingPerMonthAdapter
+import com.example.hueverianieto.ui.views.clientsbilling.ClientsBillingActivity
+import com.example.hueverianieto.ui.views.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BillingPerMonthFragment : BaseFragment() {
+
+    private lateinit var binding : FragmentBillingPerMonthBinding
+    private lateinit var currentUserData: InternalUserData
+    private lateinit var clientData: ClientData
+    private val billingViewModel: BillingPerMonthViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as BaseActivity).configNav(true)
+        this.binding = FragmentBillingPerMonthBinding.inflate(
+            inflater, container, false
+        )
+
+        val args : BillingPerMonthFragmentArgs by navArgs()
+        this.currentUserData = args.currentUserData
+        this.clientData = args.clientData
+
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        this.billingViewModel.getBillingData(clientData.documentId!!)
+        lifecycleScope.launchWhenStarted {
+            billingViewModel.viewState.collect { viewState ->
+                updateUI(viewState)
+            }
+        }
+    }
+
+    override fun setObservers() {
+        this.billingViewModel.billingContainerList.observe(this) { billingContainerList ->
+            if (billingContainerList == null) {
+                // TODO: Error
+            } else {
+                val billingList = mutableListOf<BillingContainerItemModel>()
+                for (item in billingContainerList) {
+                    if (item != null) {
+                        var billingContainerItemModel = BillingContainerItemModel(
+                            item
+                        ) {
+                            /*this.billingViewModel.navigateToBillingDetail(
+                                this.view,
+                                bundleOf(
+                                    "billingModel" to item.billingModel!!
+                                )
+                            )*/
+                        }
+                        billingList.add(billingContainerItemModel)
+                    }
+                }
+                if (billingList.isEmpty()) {
+                    // TODO: Vacío
+                } else {
+                    this.binding.billingRecyclerView.layoutManager = LinearLayoutManager(context)
+                    this.binding.billingRecyclerView.adapter =
+                        ComponentBillingPerMonthAdapter(billingList)
+                    this.binding.billingRecyclerView.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun setListeners() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        try {
+            with(state as BillingPerMonthViewState) {
+                with(binding) {
+                    this.loadingComponent.isVisible = state.isLoading
+                    if (state.error) {
+                        //setPopUp(errorMap(Constants.loginBadFormattedEmailError))
+                    } else if (state.isEmpty) {
+                        //setPopUp(errorMap(Constants.loginBadFormattedEmailError))
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, e.message.toString(), e)
+        }
+        //TODO("Not yet implemented")
+    }
+
+    companion object {
+        private val TAG = BillingPerMonthFragment::class.java.simpleName
+    }
+
+}
---
+++ app/src/main/java/com/example/hueverianieto/ui/views/clientsbilling/fragments/billingpermonth/BillingPerMonthViewModel.kt
@@ -0,0 +1,183 @@
+package com.example.hueverianieto.ui.views.clientsbilling.fragments.billingpermonth
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.findNavController
+import com.example.hueverianieto.R
+import com.example.hueverianieto.data.models.remote.OrderData
+import com.example.hueverianieto.domain.model.bilingpermonth.BillingPerMonthContainerModel
+import com.example.hueverianieto.domain.model.billingmodel.BillingModel
+import com.example.hueverianieto.domain.model.orderbillingmodel.OrderBillingModel
+import com.example.hueverianieto.domain.usecases.BillingPerMonthUseCase
+import com.example.hueverianieto.utils.Utils
+import com.google.firebase.Timestamp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class BillingPerMonthViewModel  @Inject constructor(
+    val billingUseCase: BillingPerMonthUseCase
+) : ViewModel() {
+
+    private var _viewState = MutableStateFlow(BillingPerMonthViewState())
+    val viewState: StateFlow<BillingPerMonthViewState> get() = _viewState
+
+    private val _orderList = MutableLiveData<List<OrderData?>?>()
+    val orderList: LiveData<List<OrderData?>?> get() = _orderList
+
+    private val _billingContainerList = MutableLiveData<List<BillingPerMonthContainerModel?>?>()
+    val billingContainerList: LiveData<List<BillingPerMonthContainerModel?>?> get() = _billingContainerList
+
+    fun getBillingData(documentId: String) {
+        _viewState.value = BillingPerMonthViewState(isLoading = false)
+        viewModelScope.launch {
+            _viewState.value = BillingPerMonthViewState(isLoading = true)
+            when(val result = billingUseCase(documentId)) {
+                null -> {
+                    _viewState.value = BillingPerMonthViewState(isLoading = false, error = true)
+                    _billingContainerList.value = listOf()
+                }
+                listOf<OrderData>() -> {
+                    _viewState.value = BillingPerMonthViewState(isLoading = false, isEmpty = true)
+                    _billingContainerList.value = listOf()
+                }
+                else -> {
+                    _viewState.value = BillingPerMonthViewState(isLoading = false)
+                    //_orderList.value = result
+                    val orderBillingModelList = getOrderBillingModel(result)
+                    val list = getBillingContainerFromOrderData(orderBillingModelList)
+                    _billingContainerList.value = list
+                }
+            }
+        }
+    }
+
+    private fun getOrderBillingModel(orderDataList: List<OrderData?>?) : List<OrderBillingModel> {
+        val list = mutableListOf<OrderBillingModel>()
+        if (orderDataList != null) {
+            for (item in orderDataList) {
+                if (item != null) {
+                    list.add(
+                        OrderBillingModel(
+                            item.orderId,
+                            item.orderDatetime,
+                            item.paymentMethod,
+                            item.totalPrice,
+                            item.paid
+                        )
+                    )
+                }
+            }
+        }
+        return list
+    }
+
+    private fun getBillingContainerFromOrderData(orderBillingModelList: List<OrderBillingModel>) : List<BillingPerMonthContainerModel> {
+
+        val list = mutableListOf<BillingPerMonthContainerModel>()
+        var orderBillingModelListAux = orderBillingModelList
+
+        var paymentByCash: Double = 0.0
+        var paymentByReceipt: Double = 0.0
+        var paymentByTransfer: Double = 0.0
+        var paid: Double = 0.0
+        var toBePaid: Double = 0.0
+        var totalPrice: Double = 0.0
+        var orderBillingModelMonthlyList = mutableListOf<OrderBillingModel>()
+
+        // Ordenamos la lista por fecha de pedido en desc.
+        orderBillingModelListAux = orderBillingModelListAux.sortedBy { it.orderDatetime } .reversed()
+
+        // Cogemos la primera posición -> Es la más reciente -> Último mes
+        val firstOrder = orderBillingModelListAux[0]
+        val firstDate = firstOrder.orderDatetime.toDate()
+
+        val calendar = Calendar.getInstance()
+        calendar.time = firstDate
+        var m = (calendar.get(Calendar.MONTH) + 1).toString()
+        while (m.length < 2) m = "0" + m
+        var y = calendar.get(Calendar.YEAR).toString()
+        while (y.length < 4) y = "0" + y
+
+        // Creamos fecha inicial y final
+        var initDateTimestamp = Utils.parseStringToTimestamp(
+            "01/$m/$y"
+        )
+        var endDateTimestamp = Timestamp(Utils.addToDate(initDateTimestamp.toDate(), monthsToAdd = 1))
+
+        for (item in orderBillingModelListAux) {
+            if (initDateTimestamp > item.orderDatetime) {
+                // Añadimos el elemento a la lista de retorno
+                val billingModel = BillingModel(
+                    paymentByCash = paymentByCash,
+                    paymentByReceipt = paymentByReceipt,
+                    paymentByTransfer = paymentByTransfer,
+                    paid = paid,
+                    toBePaid = toBePaid,
+                    totalPrice = totalPrice,
+                    //orderBillingModelList = orderBillingModelMonthlyList
+                )
+                val billingContainerModel = BillingPerMonthContainerModel(
+                    initDate = initDateTimestamp,
+                    endDate = endDateTimestamp,
+                    billingModel = billingModel
+                )
+                list.add(billingContainerModel)
+                // Reseteamos todas las variables y guardamos
+                endDateTimestamp = initDateTimestamp
+                initDateTimestamp = Timestamp(
+                    Utils.addToDate(initDateTimestamp.toDate(), monthsToAdd = -1))
+                paymentByCash = 0.0
+                paymentByReceipt = 0.0
+                paymentByTransfer = 0.0
+                paid = 0.0
+                toBePaid = 0.0
+                totalPrice = 0.0
+                orderBillingModelMonthlyList = mutableListOf()
+            }
+
+            // Actualizamos métodos de pago
+            when(item.paymentMethod.toInt()) {
+                0 -> paymentByCash += (item.totalPrice ?: 0).toDouble()
+                1 -> paymentByReceipt += (item.totalPrice ?: 0).toDouble()
+                2 -> paymentByTransfer += (item.totalPrice ?: 0).toDouble()
+            }
+            // Actualizamos si es un pedido pagado o por pagar
+            when(item.paid) {
+                true -> paid += (item.totalPrice ?: 0).toDouble()
+                false -> toBePaid += (item.totalPrice ?: 0).toDouble()
+            }
+            totalPrice += (item.totalPrice ?: 0).toDouble()
+            orderBillingModelMonthlyList.add(item)
+
+            if (orderBillingModelListAux.last() == item) {
+                val billingModel = BillingModel(
+                    paymentByCash = paymentByCash,
+                    paymentByReceipt = paymentByReceipt,
+                    paymentByTransfer = paymentByTransfer,
+                    paid = paid,
+                    toBePaid = toBePaid,
+                    totalPrice = totalPrice,
+                    //orderBillingModelList = orderBillingModelMonthlyList
+                )
+                val billingContainerModel = BillingPerMonthContainerModel(
+                    initDate = initDateTimestamp,
+                    endDate = endDateTimestamp,
+                    billingModel = billingModel
+                )
+                list.add(billingContainerModel)
+            }
+        }
+        return list
+    }
+
+}
