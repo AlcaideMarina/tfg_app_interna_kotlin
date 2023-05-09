c6145965bcc0bb643e03d9e84ff968642bcf62bf
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-09 20:47:16 +0200

    feat: Create billing per month container component

---
+++ app/src/main/java/com/example/hueverianieto/ui/components/componentbillingpermonth/ComponentBillingPerMonthViewHolder.kt
@@ -0,0 +1,24 @@
+package com.example.hueverianieto.ui.components.componentbillingpermonth
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hueverianieto.databinding.ComponentBillingPerMonthContainerBinding
+import com.example.hueverianieto.domain.model.billingcontaineritemmodel.BillingContainerItemModel
+import com.example.hueverianieto.utils.Utils
+
+class ComponentBillingPerMonthViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val binding: ComponentBillingPerMonthContainerBinding =
+        ComponentBillingPerMonthContainerBinding.bind(view)
+
+    fun render(billingContainerItemModel: BillingContainerItemModel) {
+        this.binding.billingDateText.text = Utils.parseDateToString(
+            billingContainerItemModel.billingPerMonthContainerModel.initDate.toDate(),
+            "MMMM, yyyy"
+        )
+        this.binding.baseComponentBillingContainer.setOnClickListener(
+            billingContainerItemModel.onClickListener
+        )
+    }
+
+}
