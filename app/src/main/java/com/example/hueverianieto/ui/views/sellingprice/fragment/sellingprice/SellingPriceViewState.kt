3efcb85fd584584014308f55a9f95b095e644c86
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-10 21:42:04 +0200

    feat: Create viewmodel and viewstate

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/sellingprice/fragment/sellingprice/SellingPriceViewState.kt
@@ -0,0 +1,8 @@
+package com.example.hueverianieto.ui.views.sellingprice.fragment.sellingprice
+
+import com.example.hueverianieto.base.BaseState
+
+class SellingPriceViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false
+) : BaseState
