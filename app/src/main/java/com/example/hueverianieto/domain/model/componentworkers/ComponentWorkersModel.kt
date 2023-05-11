8968dc3dfedc06dd4ec6b563cae6455a302c2c8a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-12 01:43:42 +0200

    feat: Create workers component

---
+++ app/src/main/java/com/example/hueverianieto/domain/model/componentworkers/ComponentWorkersModel.kt
@@ -0,0 +1,12 @@
+package com.example.hueverianieto.domain.model.componentworkers
+
+import android.view.View
+import com.example.hueverianieto.base.BaseModel
+
+class ComponentWorkersModel (
+    var id: String,
+    var name: String,
+    var surname: String,
+    var salary: Long,
+    var onClickListener: View.OnClickListener?
+) : BaseModel
---
+++ app/src/main/java/com/example/hueverianieto/ui/components/componentworker/HNComponentWorkerViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.hueverianieto.ui.components.componentworker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hueverianieto.databinding.ComponentWorkerBinding
+import com.example.hueverianieto.domain.model.componentworkers.ComponentWorkersModel
+
+class HNComponentWorkerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val binding: ComponentWorkerBinding = ComponentWorkerBinding.bind(view)
+
+    fun render(componentWorkersModel: ComponentWorkersModel){
+        this.binding.idText.text = componentWorkersModel.id
+        this.binding.nameText.text = componentWorkersModel.name + " " + componentWorkersModel.surname
+        this.binding.salaryText.text = componentWorkersModel.salary.toString() + " €"
+        this.binding.baseComponentOrderContainer.setOnClickListener(
+            componentWorkersModel.onClickListener )
+    }
+
+}
