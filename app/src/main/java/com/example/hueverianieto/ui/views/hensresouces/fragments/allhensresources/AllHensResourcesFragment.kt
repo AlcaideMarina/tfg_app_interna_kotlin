976948fd414fd50bd46b21a82805400129f29bdd
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-13 21:38:11 +0200

    feat: Add uc in vm and frg

--- app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/allhensresources/AllHensResourcesFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/allhensresources/AllHensResourcesFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.remote.HensResourcesData
 import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentAllHensResourcesBinding
+import com.example.hueverianieto.domain.model.componentclientmodel.ComponentClientBillingModel
+import com.example.hueverianieto.domain.model.componentticket.ComponentTicketModel
+import com.example.hueverianieto.ui.components.componentclientbilling.ComponentClientBillingAdapter
+import com.example.hueverianieto.ui.components.componentticket.HNComponentTicketAdapter
+import com.example.hueverianieto.ui.views.clientsbilling.fragments.clientsbilling.ClientsBillingViewState
 import com.example.hueverianieto.ui.views.hensresouces.HensResourcesActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.observeOn
 
 @AndroidEntryPoint
 class AllHensResourcesFragment : BaseFragment() {
@@ -20,11 +31,13 @@ class AllHensResourcesFragment : BaseFragment() {
     private lateinit var currentUserData: InternalUserData
     private val allHensResourcesViewModel: AllHensResourcesViewModel by viewModels()
 
+    private var hensDataList: MutableList<ComponentTicketModel> = mutableListOf()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         (activity as BaseActivity).configNav(true)
         (activity as HensResourcesActivity).getToolbar().setNavigationOnClickListener {
             (activity as BaseActivity).goBackFragments()
@@ -38,11 +51,50 @@ class AllHensResourcesFragment : BaseFragment() {
     }
 
     override fun configureUI() {
-        //TODO("Not yet implemented")
+        this.allHensResourcesViewModel.getHens()
+        lifecycleScope.launchWhenStarted {
+            allHensResourcesViewModel.getHens()
+            allHensResourcesViewModel.viewState.collect { state ->
+                updateUI(state)
+            }
+        }
     }
 
     override fun setObservers() {
-        //TODO("Not yet implemented")
+        this.allHensResourcesViewModel.hensList.observe(this) { hensResourcesDataList ->
+            if (hensResourcesDataList == null) {
+                // TODO: ERROR
+            } else {
+                hensDataList = mutableListOf()
+                for (hensResourcesData in hensResourcesDataList)  {
+                    if (hensResourcesData != null) {
+                        val componentTicketModel = ComponentTicketModel(
+                            hensResourcesData.expenseDatetime,
+                            hensResourcesData.hensNumber,
+                            "gallinas",
+                            hensResourcesData.totalPrice
+                        ) {
+                            /*this.allHensResourcesViewModel.navigateToClientBillingPerMonth(
+                                this.view,
+                                bundleOf(
+                                    "clientData" to clientData,
+                                    "currentUserData" to currentUserData
+                                )
+                            )*/
+                        }
+                        hensDataList.add(componentTicketModel)
+                    }
+                }
+                if (hensDataList.isEmpty()) {
+                    this.binding.hensRecyclerView.visibility = View.GONE
+                    this.binding.containerWaringNoOrders.visibility = View.VISIBLE
+                    this.binding.containerWaringNoOrders.setTitle("No hay clientes")
+                    this.binding.containerWaringNoOrders.setText("No hay registro de clientes activos en la base de datos")
+                } else {
+                    initRecyclerView()
+                }
+            }
+        }
     }
 
     override fun setListeners() {
@@ -50,6 +102,19 @@ class AllHensResourcesFragment : BaseFragment() {
     }
 
     override fun updateUI(state: BaseState) {
-        //TODO("Not yet implemented")
+        with(state as AllHensResourcesViewState) {
+            with(binding) {
+                this.loadingComponent.isVisible = state.isLoading
+            }
+        }
     }
+
+    private fun initRecyclerView() {
+        this.binding.hensRecyclerView.visibility = View.VISIBLE
+        this.binding.containerWaringNoOrders.visibility = View.GONE
+        this.binding.hensRecyclerView.layoutManager = LinearLayoutManager(context)
+        this.binding.hensRecyclerView.adapter = HNComponentTicketAdapter(hensDataList)
+        this.binding.hensRecyclerView.setHasFixedSize(false)
+    }
+
 }
--- app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/allhensresources/AllHensResourcesViewModel.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/allhensresources/AllHensResourcesViewModel.kt
@@ -3,23 +3,47 @@ package com.example.hueverianieto.ui.views.hensresouces.fragments.allhensresourc
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hueverianieto.data.models.remote.HensResourcesData
 import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.domain.usecases.GetHensUseCase
 import com.example.hueverianieto.domain.usecases.HomeUseCase
 import com.example.hueverianieto.ui.views.workersresources.fragment.allworkersresources.AllWorkersResourcesViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AllHensResourcesViewModel @Inject constructor(
-    val homeUseCase: HomeUseCase
+    val getHensUseCase: GetHensUseCase
 ) : ViewModel() {
 
     private val _viewState = MutableStateFlow(AllHensResourcesViewState())
     val viewState: StateFlow<AllHensResourcesViewState> get() = _viewState
 
-    private val _workerList = MutableLiveData<List<InternalUserData?>?>()
-    val workerList: LiveData<List<InternalUserData?>?> get() = _workerList
+    private val _hensList = MutableLiveData<List<HensResourcesData?>?>()
+    val hensList: LiveData<List<HensResourcesData?>?> get() = _hensList
+
+    fun getHens() {
+        viewModelScope.launch {
+            _viewState.value = AllHensResourcesViewState(isLoading = true)
+            when(val result: List<HensResourcesData?>? = getHensUseCase()) {
+                null -> {
+                    _viewState.value = AllHensResourcesViewState(isLoading = false, error = true)
+                    _hensList.value = listOf()
+                }
+                listOf<HensResourcesData>() -> {
+                    _viewState.value = AllHensResourcesViewState(isLoading = false, error = false)
+                    _hensList.value = listOf()
+                }
+                else -> {
+                    _viewState.value = AllHensResourcesViewState(isLoading = false, error = false)
+                    _hensList.value = result.sortedBy { it?.expenseDatetime }.reversed()
+                }
+            }
+        }
+    }
 
 }
