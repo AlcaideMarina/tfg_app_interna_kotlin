b772092047e098da41ce157de23fcec138462c1a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-13 22:28:10 +0200

    feat: Create modifyhensresource + add in navgraph

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/modifyhensresources/ModifyHensResourcesFragment.kt
@@ -0,0 +1,60 @@
+package com.example.hueverianieto.ui.views.hensresouces.fragments.modifyhensresources
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.remote.HensResourcesData
+import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.databinding.FragmentHensResourcesDetailBinding
+import com.example.hueverianieto.databinding.FragmentWorkersDetailBinding
+import com.example.hueverianieto.ui.components.HNModalDialog
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ModifyHensResourcesFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentHensResourcesDetailBinding
+    private lateinit var currentUserData: InternalUserData
+    private lateinit var hensResourcesData: HensResourcesData
+
+    private lateinit var alertDialog: HNModalDialog
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as BaseActivity).configNav(true)
+
+        this.alertDialog = HNModalDialog(requireContext())
+        val args: ModifyHensResourcesFragmentArgs by navArgs()
+        this.currentUserData = args.currentUserData
+        this.hensResourcesData = args.hensResourcesData
+
+        this.binding = FragmentHensResourcesDetailBinding
+            .inflate(inflater, container, false)
+
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        //TODO("Not yet implemented")
+    }
+}
