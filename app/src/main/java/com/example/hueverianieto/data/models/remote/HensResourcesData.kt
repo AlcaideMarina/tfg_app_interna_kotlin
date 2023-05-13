171859cd46c028f01569b1be1ff1406f656999fc
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-13 05:52:59 +0200

    feat: Create hens resources data

---
+++ app/src/main/java/com/example/hueverianieto/data/models/remote/HensResourcesData.kt
@@ -0,0 +1,17 @@
+package com.example.hueverianieto.data.models.remote
+
+import android.os.Parcelable
+import com.google.firebase.Timestamp
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class HensResourcesData (
+    var createdBy: String,
+    var creationDatetime: Timestamp,
+    var documentId: String?,
+    var expenseDatetime: Timestamp,
+    var hensNumber: Long,
+    var shedA: Long,
+    var shedB: Long,
+    var totalPrice: Number
+) : Parcelable
