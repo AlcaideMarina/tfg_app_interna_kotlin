c69d1dc11cb5f5bf6d0b6a4e20f7bd2395e32a97
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-13 05:53:25 +0200

    feat: Create material utils with hens functions

---
+++ app/src/main/java/com/example/hueverianieto/utils/MaterialUtils.kt
@@ -0,0 +1,35 @@
+package com.example.hueverianieto.utils
+
+import com.example.hueverianieto.data.models.remote.HensResourcesData
+import com.google.firebase.Timestamp
+
+object MaterialUtils {
+
+    fun hensMapToParcelable(data: MutableMap<String, Any?>, documentId: String?): HensResourcesData {
+        return HensResourcesData(
+            data["created_by"] as String,
+            data["creation_datetime"] as Timestamp,
+            documentId,
+            data["expense_datetime"] as Timestamp,
+            data["hens_number"] as Long,
+            data["shed_a"] as Long,
+            data["shed_b"] as Long,
+            data["total_price"] as Number,
+        )
+    }
+
+    fun hensParcelableToMap(hensResourcesData: HensResourcesData) : Map<String, Any?> {
+        val map = mutableMapOf<String, Any?>()
+
+        map["created_by"] = hensResourcesData.createdBy
+        map["creation_datetime"] = hensResourcesData.creationDatetime
+        map["expense_datetime"] = hensResourcesData.expenseDatetime
+        map["hens_number"] = hensResourcesData.hensNumber
+        map["shed_a"] = hensResourcesData.shedA
+        map["shed_b"] = hensResourcesData.shedB
+        map["total_price"] = hensResourcesData.totalPrice
+
+        return map
+    }
+
+}
