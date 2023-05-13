8387b510b4efb8d3652f2295a296ad54bfaf12dd
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-13 04:53:17 +0200

    feat: Create hens resources activity

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/HensResourcesActivity.kt
@@ -0,0 +1,55 @@
+package com.example.hueverianieto.ui.views.hensresouces
+
+import android.os.Build
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.databinding.ActivityHensResourcesBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HensResourcesActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityHensResourcesBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    lateinit var currentUserData: InternalUserData
+
+    override fun setUp() {
+        currentUserData = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra("current_user_data", InternalUserData::class.java)!!
+        } else {
+            intent.getParcelableExtra<InternalUserData>("current_user_data")!!
+        }
+
+        this.binding = ActivityHensResourcesBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        setSupportActionBar(this.binding.topBar)
+        navController = this.binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        this.binding.topBar.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf()         //TODO
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun configureUI() {
+        // Not necessary
+    }
+
+    override fun setListeners() {
+        // Not necessary
+    }
+
+    override fun setObservers() {
+        // Not necessary
+    }
+
+}
