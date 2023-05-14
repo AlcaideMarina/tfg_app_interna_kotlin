7735d862f1f285c710ec47ea335c181419aff878
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 00:58:46 +0200

    feat: Create VM + deleteFeed fun

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/feedresources/fragments/feedresoucesdetail/FeedResourcesDetailViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.hueverianieto.ui.views.feedresources.fragments.feedresoucesdetail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hueverianieto.domain.usecases.DeleteFeedResourcesUseCase
+import com.example.hueverianieto.ui.views.hensresouces.fragments.hensresourcesdetail.HensResourcesDetailViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedResourcesDetailViewModel @Inject constructor(
+    val deleteFeedResourcesUseCase: DeleteFeedResourcesUseCase
+) : ViewModel() {
+
+    private val _viewState = MutableStateFlow(FeedResourcesDetailViewState())
+    val viewState: StateFlow<FeedResourcesDetailViewState> get() = _viewState
+
+    fun deleteHenResources(documentId: String) {
+        viewModelScope.launch {
+            _viewState.value = FeedResourcesDetailViewState(isLoading = true)
+            deleteFeedResourcesUseCase(documentId)
+            _viewState.value = FeedResourcesDetailViewState(isLoading = false)
+        }
+    }
+
+}
