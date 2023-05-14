46bb7d7ffd37087dbf0181d4d899bfb1c6edf6e9
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 23:21:10 +0200

    feat: Create state

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/electricitywatergasresources/fragments/newelectricitywatergasresources/NewElectricityWaterGasResourcesViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.electricitywatergasresources.fragments.newelectricitywatergasresources
+
+import com.example.hueverianieto.base.BaseState
+
+class NewElectricityWaterGasResourcesViewState (
+    val isLoading: Boolean = false,
+    val error: Boolean = false,
+    val correct: Boolean = false
+) : BaseState
