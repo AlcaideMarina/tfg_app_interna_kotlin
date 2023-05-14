4fcb8af78db2d5ca1d39beacc4f41338e34a90a0
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 15:22:03 +0200

    feat: Create newhensresourceservice

---
+++ app/src/main/java/com/example/hueverianieto/data/services/NewDocumentService.kt
@@ -0,0 +1,18 @@
+package com.example.hueverianieto.data.services
+
+import com.google.firebase.firestore.DocumentReference
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class NewDocumentService @Inject constructor(
+    private val firebaseClient: FirebaseClient
+) {
+
+    suspend fun newDocument(collection: String, dataMap: Map<String, Any?>) : Result<DocumentReference> = runCatching {
+         firebaseClient.db
+            .collection(collection)
+            .add(dataMap)
+            .await()
+    }
+
+}
