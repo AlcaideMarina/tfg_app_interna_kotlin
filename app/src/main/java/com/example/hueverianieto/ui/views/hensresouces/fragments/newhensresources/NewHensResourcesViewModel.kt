9540d9be7997f6e49d58b3f6981da7dc43fb3496
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 16:45:32 +0200

    feat: Create addhens fun in VM

--- app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/newhensresources/NewHensResourcesViewModel.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/newhensresources/NewHensResourcesViewModel.kt
@@ -1,12 +1,53 @@
 package com.example.hueverianieto.ui.views.hensresouces.fragments.newhensresources
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hueverianieto.data.models.local.AlertOkData
+import com.example.hueverianieto.data.models.remote.HensResourcesData
 import com.example.hueverianieto.domain.usecases.NewHensResourcesUseCase
+import com.example.hueverianieto.ui.views.internalusers.fragments.newinternaluser.NewInternalUserViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NewHensResourcesViewModel @Inject constructor(
     val newHensResourcesUseCase: NewHensResourcesUseCase
 ) : ViewModel() {
+
+    private val _viewState = MutableStateFlow(NewHensResourcesViewState())
+    val viewState: StateFlow<NewHensResourcesViewState> get() = _viewState
+
+    private var _alertDialog = MutableLiveData(AlertOkData())
+    val alertDialog : LiveData<AlertOkData> get() = _alertDialog
+
+    fun addHensResource(hensResourcesData: HensResourcesData) {
+        viewModelScope.launch {
+            _viewState.value = NewHensResourcesViewState(isLoading = true)
+            when(newHensResourcesUseCase(hensResourcesData)) {
+                false -> {
+                    _viewState.value = NewHensResourcesViewState(isLoading = false, error = true)
+                    _alertDialog.value = AlertOkData(
+                        title = "Error",
+                        text = "Se ha producido un error al guardar el recurso. Por favor, revise los datos e inténtelo de nuevo.",
+                        true
+                    )
+                }
+                true -> {
+                    _viewState.value = NewHensResourcesViewState(isLoading = false)
+                    _alertDialog.value = AlertOkData(
+                        title = "Recurso guardado",
+                        text = "Las información sobre las gallinas han sido guardada correctamente en la base de datos.",
+                        true,
+                        customCode = 0
+                    )
+                }
+            }
+        }
+    }
+
 }
