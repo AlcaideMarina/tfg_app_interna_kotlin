1ba2aec391133cc89a25a1bc6cae7d498a7434ec
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 19:26:18 +0200

    feat: Show EWG in RV

--- app/src/main/java/com/example/hueverianieto/ui/views/electricitywatergasresources/fragments/allelectricitywatergas/AllElectricityWaterGasFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/electricitywatergasresources/fragments/allelectricitywatergas/AllElectricityWaterGasFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
@@ -14,6 +16,7 @@ import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentAllElectricityWaterGasResourcesBinding
 import com.example.hueverianieto.databinding.FragmentAllHensResourcesBinding
 import com.example.hueverianieto.domain.model.componentticket.ComponentTicketModel
+import com.example.hueverianieto.ui.components.componentticket.HNComponentTicketAdapter
 import com.example.hueverianieto.ui.views.electricitywatergasresources.ElectricityWaterGasResourcesActivity
 import com.example.hueverianieto.ui.views.hensresouces.HensResourcesActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -55,7 +58,34 @@ class AllElectricityWaterGasFragment : BaseFragment() {
     }
 
     override fun setObservers() {
-        //TODO("Not yet implemented")
+        this.allEWGViewModel.ewgList.observe(this) { ewgResourcesDataList ->
+            if (ewgResourcesDataList == null) {
+                // TODO: Error
+            } else {
+                ewgDataList = mutableListOf()
+                for (ewgResourcesData in ewgResourcesDataList)  {
+                    if (ewgResourcesData != null) {
+                        val componentTicketModel = ComponentTicketModel(
+                            ewgResourcesData.expenseDatetime,
+                            ewgResourcesData.type.toString(),       // TOOD: ESTO HAY QUE PASARLO POR UN MAP
+                            "",
+                            ewgResourcesData.totalPrice
+                        ) {
+                            // TODO: Navegación
+                        }
+                        ewgDataList.add(componentTicketModel)
+                    }
+                }
+                if (ewgDataList.isEmpty()) {
+                    this.binding.electricityWaterGasRecyclerView.visibility = View.GONE
+                    this.binding.containerWarningNoTickets.visibility = View.VISIBLE
+                    this.binding.containerWarningNoTickets.setTitle("No hay clientes")
+                    this.binding.containerWarningNoTickets.setText("No hay registro de clientes activos en la base de datos")
+                } else {
+                    initRecyclerView()
+                }
+            }
+        }
     }
 
     override fun setListeners() {
@@ -68,6 +98,14 @@ class AllElectricityWaterGasFragment : BaseFragment() {
                 this.loadingComponent.isVisible = state.isLoading
             }
         }
+    }
+
+    private fun initRecyclerView() {
+        this.binding.electricityWaterGasRecyclerView.visibility = View.VISIBLE
+        this.binding.containerWarningNoTickets.visibility = View.GONE
+        this.binding.electricityWaterGasRecyclerView.layoutManager = LinearLayoutManager(context)
+        this.binding.electricityWaterGasRecyclerView.adapter = HNComponentTicketAdapter(ewgDataList)
+        this.binding.electricityWaterGasRecyclerView.setHasFixedSize(false)
     }
 
 }
