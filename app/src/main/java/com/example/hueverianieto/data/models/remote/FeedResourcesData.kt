d0e6dccade1f3ea95387c535cc8bf661c081d3c4
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 00:16:16 +0200

    feat: Create feed data model

---
+++ app/src/main/java/com/example/hueverianieto/data/models/remote/FeedResourcesData.kt
@@ -0,0 +1,15 @@
+package com.example.hueverianieto.data.models.remote
+
+import android.os.Parcelable
+import com.google.firebase.Timestamp
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class FeedResourcesData (
+    var createdBy: String,
+    var creationDatetime: Timestamp,
+    var deleted: Boolean,
+    var expenseDatetime: Timestamp,
+    var kilos: Number,
+    val totalPrice: Number
+) : Parcelable
