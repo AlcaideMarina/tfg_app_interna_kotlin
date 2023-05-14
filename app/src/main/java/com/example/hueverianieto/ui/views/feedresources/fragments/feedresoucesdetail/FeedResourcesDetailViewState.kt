250882a8b11b43d32e63692a6b951cb02144f06d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 00:58:18 +0200

    feat: Create ViewState

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/feedresources/fragments/feedresoucesdetail/FeedResourcesDetailViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.feedresources.fragments.feedresoucesdetail
+
+import com.example.hueverianieto.base.BaseState
+
+class FeedResourcesDetailViewState (
+    val isLoading: Boolean = false,
+    val error: Boolean = false,
+    val correct: Boolean = false
+) : BaseState
