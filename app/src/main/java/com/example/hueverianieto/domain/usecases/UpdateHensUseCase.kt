462f135df1f6516d0b2f48f118a6d3c7bf7a1692
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 13:02:19 +0200

    feat: Create update hens UC

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/UpdateHensUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.services.UpdateFirestoreService
+import javax.inject.Inject
+
+class UpdateHensUseCase @Inject constructor(
+    private val updateFirestoreService: UpdateFirestoreService
+) {
+
+    suspend operator fun invoke(hensDataMap: Map<String, Any?>, documentId: String) : Boolean =
+        updateFirestoreService.updateClientFirestore(hensDataMap, documentId, "material_hens")
+
+
+}
