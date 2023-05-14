0a27ffcb1e5618f8d9139e7ce54fc73faa84b151
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 01:31:26 +0200

    feat: Create getFeed UC

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/UpdateFeedUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.services.UpdateFirestoreService
+import javax.inject.Inject
+
+class UpdateFeedUseCase @Inject constructor(
+    private val updateFirestoreService: UpdateFirestoreService
+) {
+
+    suspend fun invoke(feedDataMap: Map<String, Any?>, documentId: String) : Boolean =
+        updateFirestoreService.updateDocumentFirestore(feedDataMap, documentId, "material_feed")
+
+}
