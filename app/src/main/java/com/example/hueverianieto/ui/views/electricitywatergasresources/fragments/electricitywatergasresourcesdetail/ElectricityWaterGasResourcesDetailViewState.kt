47899fcaa9d8c3ba4e0a71cf025045f0592eecad
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 20:25:55 +0200

    feat: Create viewstate

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/electricitywatergasresources/fragments/electricitywatergasresourcesdetail/ElectricityWaterGasResourcesDetailViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.electricitywatergasresources.fragments.electricitywatergasresourcesdetail
+
+import com.example.hueverianieto.base.BaseState
+
+class ElectricityWaterGasResourcesDetailViewState (
+    val isLoading: Boolean = false,
+    val error: Boolean = false,
+    val correct: Boolean = false
+) : BaseState
