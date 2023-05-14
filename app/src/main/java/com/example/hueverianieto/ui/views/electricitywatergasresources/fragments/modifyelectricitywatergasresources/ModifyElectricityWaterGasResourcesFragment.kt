cd1bcc41583c5d06586891305bc68c688039070a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 20:46:57 +0200

    feat: Create modify ewg frg

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/electricitywatergasresources/fragments/modifyelectricitywatergasresources/ModifyElectricityWaterGasResourcesFragment.kt
@@ -0,0 +1,57 @@
+package com.example.hueverianieto.ui.views.electricitywatergasresources.fragments.modifyelectricitywatergasresources
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.remote.ElectricityWaterGasResourcesData
+import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.databinding.FragmentElectricityWaterGasResourcesDetailBinding
+import com.example.hueverianieto.databinding.FragmentHensResourcesDetailBinding
+import com.example.hueverianieto.ui.components.HNModalDialog
+import com.example.hueverianieto.ui.views.hensresouces.fragments.modifyhensresources.ModifyHensResourcesFragmentArgs
+
+class ModifyElectricityWaterGasResourcesFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentElectricityWaterGasResourcesDetailBinding
+    private lateinit var currentUserData: InternalUserData
+    private lateinit var ewgResourcesData: ElectricityWaterGasResourcesData
+
+    private lateinit var alertDialog: HNModalDialog
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as BaseActivity).configNav(true)
+
+        this.alertDialog = HNModalDialog(requireContext())
+
+        this.binding = FragmentElectricityWaterGasResourcesDetailBinding
+            .inflate(inflater, container, false)
+
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        TODO("Not yet implemented")
+    }
+
+}
