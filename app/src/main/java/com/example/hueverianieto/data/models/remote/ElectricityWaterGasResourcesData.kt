529e83e92b038d4ed19e82bcabb2ce24f8966bd0
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 18:41:36 +0200

    feat: Create EWG model

---
+++ app/src/main/java/com/example/hueverianieto/data/models/remote/ElectricityWaterGasResourcesData.kt
@@ -0,0 +1,16 @@
+package com.example.hueverianieto.data.models.remote
+
+import android.os.Parcelable
+import com.google.firebase.Timestamp
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class ElectricityWaterGasResourcesData (
+    var createBy: String,
+    var creationDatatime: Timestamp,
+    var deleted: Boolean,
+    var documentId: String?,
+    var expenseDatetime: Timestamp,
+    var notes: String,
+    var totalPrice: Number
+) : Parcelable
