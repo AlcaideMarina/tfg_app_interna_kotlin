d53a4f158a36f5e9b7efee04016fbab5cb047ed6
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 16:45:08 +0200

    feat: Create NewHensResourcesViewState

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/hensresouces/fragments/newhensresources/NewHensResourcesViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.hensresouces.fragments.newhensresources
+
+import com.example.hueverianieto.base.BaseState
+
+class NewHensResourcesViewState (
+    val isLoading: Boolean = false,
+    val error: Boolean = false,
+    val correct: Boolean = false
+) : BaseState
