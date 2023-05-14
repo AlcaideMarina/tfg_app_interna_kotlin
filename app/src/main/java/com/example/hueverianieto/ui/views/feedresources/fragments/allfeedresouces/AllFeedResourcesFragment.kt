7ca8819c8408bd1d1b7ec5bbc63e897d466880d3
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 00:38:06 +0200

    feat: Set RV

--- app/src/main/java/com/example/hueverianieto/ui/views/feedresources/fragments/allfeedresouces/AllFeedResourcesFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/feedresources/fragments/allfeedresouces/AllFeedResourcesFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
@@ -14,6 +16,7 @@ import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentAllFeedResourcesBinding
 import com.example.hueverianieto.databinding.FragmentAllHensResourcesBinding
 import com.example.hueverianieto.domain.model.componentticket.ComponentTicketModel
+import com.example.hueverianieto.ui.components.componentticket.HNComponentTicketAdapter
 import com.example.hueverianieto.ui.views.feedresources.FeedResourcesActivity
 import com.example.hueverianieto.ui.views.hensresouces.HensResourcesActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -56,7 +59,34 @@ class AllFeedResourcesFragment : BaseFragment() {
     }
 
     override fun setObservers() {
-        //TODO("Not yet implemented")
+        this.allFeedResourcesViewModel.feedList.observe(this) { feedResourcesDataList ->
+            if (feedResourcesDataList == null) {
+                // TODO: ERROR
+            } else {
+                feedDataList = mutableListOf()
+                for (hensResourcesData in feedResourcesDataList)  {
+                    if (hensResourcesData != null) {
+                        val componentTicketModel = ComponentTicketModel(
+                            hensResourcesData.expenseDatetime,
+                            hensResourcesData.kilos.toString(),
+                            "kg",
+                            hensResourcesData.totalPrice
+                        ) {
+                            // TODO: Navegación
+                        }
+                        feedDataList.add(componentTicketModel)
+                    }
+                }
+                if (feedDataList.isEmpty()) {
+                    this.binding.feedRecyclerView.visibility = View.GONE
+                    this.binding.containerWaringNoOrders.visibility = View.VISIBLE
+                    this.binding.containerWaringNoOrders.setTitle("No hay clientes")
+                    this.binding.containerWaringNoOrders.setText("No hay registro de clientes activos en la base de datos")
+                } else {
+                    initRecyclerView()
+                }
+            }
+        }
     }
 
     override fun setListeners() {
@@ -67,5 +97,13 @@ class AllFeedResourcesFragment : BaseFragment() {
         with(state as AllFeedResourcesViewState) {
             binding.loadingComponent.isVisible = state.isLoading
         }
+    }
+
+    private fun initRecyclerView() {
+        this.binding.feedRecyclerView.visibility = View.VISIBLE
+        this.binding.containerWaringNoOrders.visibility = View.GONE
+        this.binding.feedRecyclerView.layoutManager = LinearLayoutManager(context)
+        this.binding.feedRecyclerView.adapter = HNComponentTicketAdapter(feedDataList)
+        this.binding.feedRecyclerView.setHasFixedSize(false)
     }
 }
