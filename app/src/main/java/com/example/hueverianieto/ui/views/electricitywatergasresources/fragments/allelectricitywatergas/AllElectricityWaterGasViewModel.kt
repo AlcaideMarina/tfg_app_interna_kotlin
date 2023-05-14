f022f90eed1704012ad627a31812e5b4786ce21d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 19:12:49 +0200

    feat: Create VM + getEWG fun

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/electricitywatergasresources/fragments/allelectricitywatergas/AllElectricityWaterGasViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.hueverianieto.ui.views.electricitywatergasresources.fragments.allelectricitywatergas
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hueverianieto.data.models.remote.ElectricityWaterGasResourcesData
+import com.example.hueverianieto.data.models.remote.HensResourcesData
+import com.example.hueverianieto.domain.usecases.GetEWGUseCase
+import com.example.hueverianieto.ui.views.hensresouces.fragments.allhensresources.AllHensResourcesViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AllElectricityWaterGasViewModel @Inject constructor(
+    val getEWGUseCase: GetEWGUseCase
+) : ViewModel() {
+
+    private val _viewState = MutableStateFlow(AllElectricityWaterGasViewState())
+    val viewState: StateFlow<AllElectricityWaterGasViewState> get() = _viewState
+
+    private val _ewgList = MutableLiveData<List<ElectricityWaterGasResourcesData?>?>()
+    val ewgList: LiveData<List<ElectricityWaterGasResourcesData?>?> get() = _ewgList
+
+    fun getEWG() {
+        viewModelScope.launch {
+            _viewState.value = AllElectricityWaterGasViewState(isLoading = true)
+            when(val result: List<ElectricityWaterGasResourcesData?>? = getEWGUseCase()) {
+                null -> {
+                    _viewState.value = AllElectricityWaterGasViewState(isLoading = false, error = true)
+                    _ewgList.value = listOf()
+                }
+                listOf<HensResourcesData>() -> {
+                    _viewState.value = AllElectricityWaterGasViewState(isLoading = false, error = false)
+                    _ewgList.value = listOf()
+                }
+                else -> {
+                    _viewState.value = AllElectricityWaterGasViewState(isLoading = false, error = false)
+                    _ewgList.value = result.sortedBy { it?.expenseDatetime }.reversed()
+                }
+            }
+        }
+    }
+
+}
