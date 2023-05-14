57d42e5190722239b9c8ad31b1041c1ec4b76a3b
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-14 20:34:47 +0200

    feat: Create UC to update after update

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/GetEWGResourcesWithIdUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.models.remote.ElectricityWaterGasResourcesData
+import com.example.hueverianieto.data.services.GetFirestoreDocumentWithDocIdService
+import com.example.hueverianieto.utils.MaterialUtils
+import javax.inject.Inject
+
+class GetEWGResourcesWithIdUseCase @Inject constructor(
+    private val getFirestoreDocumentWithDocIdService: GetFirestoreDocumentWithDocIdService
+) {
+
+    suspend operator fun invoke(documentId: String) : ElectricityWaterGasResourcesData? {
+        return when(val result = getFirestoreDocumentWithDocIdService.getDocumentWithDocId("material_hens", documentId).getOrNull()) {
+            null -> null
+            else -> {
+                if (result.data != null) {
+                    MaterialUtils.ewgMapToParcelable(result.data!!, result.id)
+                } else null
+            }
+        }
+    }
+
+}
