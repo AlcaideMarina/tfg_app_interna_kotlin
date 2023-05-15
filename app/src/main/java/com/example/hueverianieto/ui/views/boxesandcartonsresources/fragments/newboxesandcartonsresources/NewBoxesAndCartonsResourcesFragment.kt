1698f185b43d1fd949f13ed7de52726808700e42
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 13:57:27 +0200

    feat: Set UI

--- app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/newboxesandcartonsresources/NewBoxesAndCartonsResourcesFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/newboxesandcartonsresources/NewBoxesAndCartonsResourcesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.hueverianieto.ui.views.boxesandcartonsresources.fragments.newboxesandcartonsresources
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,8 +12,10 @@ import com.example.hueverianieto.base.BaseState
 import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentNewBoxesAndCartonsResourcesBinding
 import com.example.hueverianieto.ui.components.HNModalDialog
+import com.example.hueverianieto.utils.Utils
 import com.google.firebase.Timestamp
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class NewBoxesAndCartonsResourcesFragment : BaseFragment() {
@@ -42,7 +45,8 @@ class NewBoxesAndCartonsResourcesFragment : BaseFragment() {
     }
 
     override fun configureUI() {
-        //TODO("Not yet implemented")
+        setButtons()
+        setFields()
     }
 
     override fun setObservers() {
@@ -56,4 +60,42 @@ class NewBoxesAndCartonsResourcesFragment : BaseFragment() {
     override fun updateUI(state: BaseState) {
         //TODO("Not yet implemented")
     }
+
+    private fun setButtons() {
+        with(this.binding) {
+            this.cancelButton.setText("Cancelar")
+            this.saveButton.setText("Guardar")
+        }
+    }
+
+    private fun setFields() {
+        with(this.binding) {
+            datetimeSelected = Timestamp(Date())
+            this.dateTextInputLayout.setText(
+                Utils.parseDateToString(datetimeSelected.toDate())
+            )
+            this.dateTextInputLayout.setOnClickListener { onClickScheduledDate() }
+        }
+    }
+
+    private fun onClickScheduledDate() {
+        val selectedCalendar = Calendar.getInstance()
+        val year = selectedCalendar.get(Calendar.YEAR)
+        val month = selectedCalendar.get(Calendar.MONTH)
+        val day = selectedCalendar.get(Calendar.DATE)
+        val listener = DatePickerDialog.OnDateSetListener { _, y, m, d ->
+            var dayStr = d.toString()
+            var monthStr = (m + 1).toString()
+            var yearStr = y.toString()
+            if (dayStr.length < 2) dayStr = "0$dayStr"
+            if (monthStr.length < 2) monthStr = "0$monthStr"
+            if (yearStr.length < 4) yearStr = "0$yearStr"
+            this.binding.dateTextInputLayout.setText("$dayStr/$monthStr/$yearStr")
+            datetimeSelected = Utils.parseStringToTimestamp("$dayStr/$monthStr/$yearStr")
+        }
+        val datePickerDialog = DatePickerDialog(requireContext(), listener, year, month, day)
+        datePickerDialog.datePicker.maxDate = Date().time
+        datePickerDialog.show()
+    }
+
 }
