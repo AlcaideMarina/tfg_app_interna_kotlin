449bb65ff9fdf1b75ad993b83a7d84abb103002d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 19:46:25 +0200

    feat: Create container data model

---
+++ app/src/main/java/com/example/hueverianieto/domain/model/finalproductcontrol/FPCContainerItemModel.kt
@@ -0,0 +1,8 @@
+package com.example.hueverianieto.domain.model.finalproductcontrol
+
+import android.view.View.OnClickListener
+
+data class FPCContainerItemModel (
+    var monthlyFPCContainerModel: MonthlyFPCContainerModel,
+    var onClickListener: OnClickListener
+)
