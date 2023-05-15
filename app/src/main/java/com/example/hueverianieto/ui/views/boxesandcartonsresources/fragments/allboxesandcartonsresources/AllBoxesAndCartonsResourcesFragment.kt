e7c6f03bd8c585fd5eaf4c9172b50b7496d394a7
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 03:22:17 +0200

    feat: Add VM in frg

--- app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/allboxesandcartonsresources/AllBoxesAndCartonsResourcesFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/allboxesandcartonsresources/AllBoxesAndCartonsResourcesFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
@@ -13,11 +16,13 @@ import com.example.hueverianieto.databinding.FragmentAllFeedResourcesBinding
 import com.example.hueverianieto.domain.model.componentticket.ComponentTicketModel
 import com.example.hueverianieto.ui.views.boxesandcartonsresources.BoxesAndCartonsActivity
 import com.example.hueverianieto.ui.views.feedresources.FeedResourcesActivity
+import kotlinx.coroutines.flow.collect
 
 class AllBoxesAndCartonsResourcesFragment : BaseFragment() {
 
     private lateinit var binding: FragmentAllBoxesAndCartonsResourcesBinding
     private lateinit var currentUserData: InternalUserData
+    private val allBoxesAndCartonsResourcesViewModel: AllBoxesAndCartonsResourcesViewModel by viewModels()
 
     private var boxesAndCartonsDataList: MutableList<ComponentTicketModel> = mutableListOf()
 
@@ -39,7 +44,13 @@ class AllBoxesAndCartonsResourcesFragment : BaseFragment() {
     }
 
     override fun configureUI() {
-        //TODO("Not yet implemented")
+        this.allBoxesAndCartonsResourcesViewModel.getCB()
+        lifecycleScope.launchWhenStarted {
+            allBoxesAndCartonsResourcesViewModel.getCB()
+            allBoxesAndCartonsResourcesViewModel.viewState.collect { viewState ->
+                updateUI(viewState)
+            }
+        }
     }
 
     override fun setObservers() {
@@ -51,7 +62,9 @@ class AllBoxesAndCartonsResourcesFragment : BaseFragment() {
     }
 
     override fun updateUI(state: BaseState) {
-        //TODO("Not yet implemented")
+        with(state as AllBoxesAndCartonsResourcesViewState) {
+            binding.loadingComponent.isVisible = state.isLoading
+        }
     }
 
 }
