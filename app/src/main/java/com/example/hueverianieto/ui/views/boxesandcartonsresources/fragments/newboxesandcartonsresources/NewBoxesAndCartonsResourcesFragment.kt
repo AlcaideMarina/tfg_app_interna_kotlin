fa53cdfa7c21c752435994fcd7e61b97a213566a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 14:22:43 +0200

    feat: Save new order

--- app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/newboxesandcartonsresources/NewBoxesAndCartonsResourcesFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/newboxesandcartonsresources/NewBoxesAndCartonsResourcesFragment.kt
@@ -12,10 +12,14 @@ import androidx.navigation.fragment.navArgs
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.local.DBBoxesAndCartonsOrderFieldData
+import com.example.hueverianieto.data.models.remote.BoxesAndCartonsResourcesData
+import com.example.hueverianieto.data.models.remote.FeedResourcesData
 import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentNewBoxesAndCartonsResourcesBinding
 import com.example.hueverianieto.domain.usecases.NewBoxesAndCartonsResourcesUseCase
 import com.example.hueverianieto.ui.components.HNModalDialog
+import com.example.hueverianieto.utils.MaterialUtils
 import com.example.hueverianieto.utils.Utils
 import com.google.firebase.Timestamp
 import dagger.hilt.android.AndroidEntryPoint
@@ -69,6 +73,43 @@ class NewBoxesAndCartonsResourcesFragment : BaseFragment() {
             it.hideSoftInput()
             activity?.onBackPressedDispatcher?.onBackPressed()
         }
+        this.binding.saveButton.setOnClickListener {
+            it.hideSoftInput()
+            val orderFieldStructure = getOrderFieldStructure()
+            val emptyOrder = DBBoxesAndCartonsOrderFieldData(
+                (0).toLong(), (0).toLong(), (0).toLong(), (0).toLong(), (0).toLong())
+            if (this.binding.dateTextInputLayout.text != null && this.binding.dateTextInputLayout.text.toString() != "" &&
+                orderFieldStructure != emptyOrder &&
+                this.binding.totalPriceTextInputLayout.text != null && this.binding.totalPriceTextInputLayout.text.toString() != "") {
+                it.hideSoftInput()
+                val bcResourcesData = BoxesAndCartonsResourcesData(
+                    this.currentUserData.documentId!!,
+                    Timestamp.now(),
+                    false,
+                    null,
+                    datetimeSelected,
+                    MaterialUtils
+                        .parseDBBoxesAndCartonsOrderFieldDataToMap(getOrderFieldStructure()),
+                    this.binding.totalPriceTextInputLayout.text.toString().toDouble()
+                )
+
+                this.newBoxesAndCartonsResourcesViewModel.addBoxesAndCartonsResource(
+                    bcResourcesData
+                )
+                activity?.onBackPressedDispatcher?.onBackPressed()
+            } else {
+                Utils.setPopUp(
+                    alertDialog,
+                    requireContext(),
+                    "Formulario incompleto",
+                    "Debe rellenar todos los campos del formulario. Por favor, revise lod datos e inténtelo de nuevo.",
+                    "De acuerdo",
+                    null,
+                    { alertDialog.cancel() },
+                    null
+                )
+            }
+        }
     }
 
     override fun updateUI(state: BaseState) {
@@ -112,6 +153,31 @@ class NewBoxesAndCartonsResourcesFragment : BaseFragment() {
         val datePickerDialog = DatePickerDialog(requireContext(), listener, year, month, day)
         datePickerDialog.datePicker.maxDate = Date().time
         datePickerDialog.show()
+    }
+
+    private fun getOrderFieldStructure() : DBBoxesAndCartonsOrderFieldData {
+        val box =
+            if (this.binding.boxesTextInputLayout.text == null || this.binding.boxesTextInputLayout.text.toString() == "") "0"
+            else this.binding.boxesTextInputLayout.text.toString()
+        val xlCartons =
+            if (this.binding.xlCartonsTextInputLayout.text == null || this.binding.xlCartonsTextInputLayout.text.toString() == "") "0"
+            else this.binding.xlCartonsTextInputLayout.text.toString()
+        val lCartons =
+            if (this.binding.lCartonsTextInputLayout.text == null || this.binding.lCartonsTextInputLayout.text.toString() == "") "0"
+            else this.binding.lCartonsTextInputLayout.text.toString()
+        val mCartons =
+            if (this.binding.mCartonsTextInputLayout.text == null || this.binding.mCartonsTextInputLayout.text.toString() == "") "0"
+            else this.binding.mCartonsTextInputLayout.text.toString()
+        val sCartons =
+            if (this.binding.sCartonsTextInputLayout.text == null || this.binding.sCartonsTextInputLayout.text.toString() == "") "0"
+            else this.binding.sCartonsTextInputLayout.text.toString()
+        return DBBoxesAndCartonsOrderFieldData(
+            box = box.toLong(),
+            xlCarton = xlCartons.toLong(),
+            lCarton = lCartons.toLong(),
+            mCarton = mCartons.toLong(),
+            sCarton = sCartons.toLong(),
+        )
     }
 
 }
