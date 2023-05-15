1cb34ca0769c483d9b8fbb88c0f59aaf92285b43
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 19:46:14 +0200

    feat: Create monthly data model

---
+++ app/src/main/java/com/example/hueverianieto/domain/model/finalproductcontrol/MonthlyFPCContainerModel.kt
@@ -0,0 +1,10 @@
+package com.example.hueverianieto.domain.model.finalproductcontrol
+
+import com.example.hueverianieto.data.models.remote.FPCData
+import com.google.firebase.Timestamp
+
+data class MonthlyFPCContainerModel(
+    val initDate: Timestamp,
+    var endDate: Timestamp,
+    var fpcData: FPCData
+)
