9413b58281f89f9b82a9fc7a7709d5be74aafe46
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 04:27:19 +0200

    feat: Create ViewState

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/boxesandcartonsresourcesdetail/BoxesAndCartonsResourcesDetailViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.boxesandcartonsresources.fragments.boxesandcartonsresourcesdetail
+
+import com.example.hueverianieto.base.BaseState
+
+class BoxesAndCartonsResourcesDetailViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false,
+    var isEmpty: Boolean = false
+) : BaseState
