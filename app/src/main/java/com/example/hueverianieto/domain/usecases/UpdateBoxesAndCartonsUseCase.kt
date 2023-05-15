db52b49cf737a6aaea27e3480b4181f9f186475d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 12:32:58 +0200

    feat: Create update UC

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/UpdateBoxesAndCartonsUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.services.UpdateFirestoreService
+import javax.inject.Inject
+
+class UpdateBoxesAndCartonsUseCase @Inject constructor(
+    private val updateFirestoreService: UpdateFirestoreService
+) {
+
+    suspend operator fun invoke(bcDataMpa: Map<String, Any?>, documentId: String) : Boolean =
+        updateFirestoreService.updateDocumentFirestore(bcDataMpa, documentId, "material_boxes_and_cartons")
+
+}
