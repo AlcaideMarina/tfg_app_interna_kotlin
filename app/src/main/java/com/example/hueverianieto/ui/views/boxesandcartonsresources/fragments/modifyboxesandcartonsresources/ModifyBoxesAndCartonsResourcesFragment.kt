352e0335f24387aa707ac7ba0fd177e5e7a307c7
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 04:47:51 +0200

    feat: Add modify frg to navgraph

--- app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/modifyboxesandcartonsresources/ModifyBoxesAndCartonsResourcesFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/boxesandcartonsresources/fragments/modifyboxesandcartonsresources/ModifyBoxesAndCartonsResourcesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
@@ -30,6 +31,10 @@ class ModifyBoxesAndCartonsResourcesFragment : BaseFragment() {
         (activity as BaseActivity).configNav(true)
 
         this.alertDialog = HNModalDialog(requireContext())
+
+        val args: ModifyBoxesAndCartonsResourcesFragmentArgs by navArgs()
+        this.currentUserData = args.currentUserData
+        this.bcResourcesData = args.bcResourcesData
 
         this.binding = FragmentBoxesAndCartonsResourcesDetailBinding
             .inflate(inflater, container, false)
