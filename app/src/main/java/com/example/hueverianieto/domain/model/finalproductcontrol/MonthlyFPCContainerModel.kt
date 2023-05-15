cdd55899ed4eab1cb244698e5ac0f2e970063cf0
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 23:35:34 +0200

    feat: Set datamodel as parcelable

--- app/src/main/java/com/example/hueverianieto/domain/model/finalproductcontrol/MonthlyFPCContainerModel.kt
+++ app/src/main/java/com/example/hueverianieto/domain/model/finalproductcontrol/MonthlyFPCContainerModel.kt
@@ -1,10 +1,13 @@
 package com.example.hueverianieto.domain.model.finalproductcontrol
 
+import android.os.Parcelable
 import com.example.hueverianieto.data.models.remote.FPCData
 import com.google.firebase.Timestamp
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class MonthlyFPCContainerModel(
     val initDate: Timestamp,
     var endDate: Timestamp,
     var fpcDataList: List<FPCData>
-)
+) : Parcelable
