dbeb72d9e68e8866bde56f0721de00e1bd768e2a
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 04:30:34 +0200

    feat: Create deleteB&C UC

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/DeleteBoxesAndCartonsResourcesUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.services.DeleteDocumentWithDocIdService
+import javax.inject.Inject
+
+class DeleteBoxesAndCartonsResourcesUseCase @Inject constructor(
+    private val deleteDocumentWithDocIdService: DeleteDocumentWithDocIdService
+) {
+
+    suspend operator fun invoke(documentId: String) =
+        deleteDocumentWithDocIdService.deleteDocument("material_boxes_anc_cartons", documentId)
+
+}
