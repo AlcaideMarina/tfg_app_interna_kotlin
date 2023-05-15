abeb864c68f6d881841f4637486ec619b5bb57f7
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-15 19:46:35 +0200

    feat: Create viewstate

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/finalproductcontrol/fragments/MonthlyFinalProductControlViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.finalproductcontrol.fragments
+
+import com.example.hueverianieto.base.BaseState
+
+class MonthlyFinalProductControlViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false,
+    var isEmpty: Boolean =  false
+) : BaseState
