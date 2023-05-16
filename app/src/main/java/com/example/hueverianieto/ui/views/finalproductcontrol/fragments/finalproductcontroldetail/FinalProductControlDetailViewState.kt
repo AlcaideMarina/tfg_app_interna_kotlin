bb0c449f25d2438241c3c60e68a4bd9a4f0e61aa
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-16 19:15:09 +0200

    feat: Create viewstate

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/finalproductcontrol/fragments/finalproductcontroldetail/FinalProductControlDetailViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.finalproductcontrol.fragments.finalproductcontroldetail
+
+import com.example.hueverianieto.base.BaseState
+
+class FinalProductControlDetailViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false,
+    var isEmpty: Boolean =  false
+) : BaseState
