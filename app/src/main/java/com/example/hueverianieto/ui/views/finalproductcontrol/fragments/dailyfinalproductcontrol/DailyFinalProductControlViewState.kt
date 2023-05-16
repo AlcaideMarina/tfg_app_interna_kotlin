ae84070f5e1d6aa274514bb9174b582c36ee8a8b
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-17 00:03:30 +0200

    feat: Create daily viewstate

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/finalproductcontrol/fragments/dailyfinalproductcontrol/DailyFinalProductControlViewState.kt
@@ -0,0 +1,9 @@
+package com.example.hueverianieto.ui.views.finalproductcontrol.fragments.dailyfinalproductcontrol
+
+import com.example.hueverianieto.base.BaseState
+
+class DailyFinalProductControlViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false,
+    var isEmpty: Boolean =  false
+) : BaseState
