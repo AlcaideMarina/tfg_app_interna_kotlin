02d7ff4995bed941d65c6b746d1c2aa2628e3b86
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-19 01:44:16 +0200

    feat: Create daily detail

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/monitoringcompanysituation/fragments/dailymonitoringcompanysituation/DailyMonitoringCompanySituationFragment.kt
@@ -0,0 +1,44 @@
+package com.example.hueverianieto.ui.views.monitoringcompanysituation.fragments.dailymonitoringcompanysituation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.databinding.FragmentDailyFinalProductControlBinding
+
+class DailyMonitoringCompanySituationFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentDailyFinalProductControlBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as BaseActivity).configNav(true)
+
+        this.binding = FragmentDailyFinalProductControlBinding.inflate(
+            inflater, container, false
+        )
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        TODO("Not yet implemented")
+    }
+}
