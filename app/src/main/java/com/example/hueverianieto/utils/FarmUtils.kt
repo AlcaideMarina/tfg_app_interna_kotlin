1c6d40f58ce69ee431f1294bdd04f5a3495800bf
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-19 00:52:25 +0200

    feat: Add parse in farmutils

--- app/src/main/java/com/example/hueverianieto/utils/FarmUtils.kt
+++ app/src/main/java/com/example/hueverianieto/utils/FarmUtils.kt
@@ -2,6 +2,7 @@ package com.example.hueverianieto.utils
 
 import com.example.hueverianieto.data.models.remote.FPCData
 import com.example.hueverianieto.data.models.remote.HensResourcesData
+import com.example.hueverianieto.data.models.remote.MonitoringCompanySituationData
 import com.google.firebase.Timestamp
 
 object FarmUtils {
@@ -35,6 +36,37 @@ object FarmUtils {
         map["lot"] = fpcData.lot
         map["packing_datetime"] = fpcData.packingDatetime
         map["rejected_eggs"] = fpcData.rejectedEggs
+
+        return map
+    }
+
+    fun monitoringCompanySituationMapToParcelable(data: MutableMap<String, Any?>, documentId: String?): MonitoringCompanySituationData {
+        return MonitoringCompanySituationData(
+            data["broken_eggs"] as Long,
+            data["created_by"] as String,
+            data["creation_datetime"] as Timestamp,
+            documentId,
+            data["hens"] as Map<String, Long>,
+            data["l_eggs"] as Map<String, Long>,
+            data["m_eggs"] as Map<String, Long>,
+            data["s_eggs"] as Map<String, Long>,
+            data["situation_datetime"] as Timestamp,
+            data["l_eggs"] as Map<String, Long>,
+        )
+    }
+
+    fun monitoringCompanySituationParcelableToMap(monitoringCompanySituationData: MonitoringCompanySituationData) : Map<String, Any?> {
+        val map = mutableMapOf<String, Any?>()
+
+        map["broken_eggs"] = monitoringCompanySituationData.brokenEggs
+        map["created_by"] = monitoringCompanySituationData.createdBy
+        map["creation_datetime"] = monitoringCompanySituationData.creationDatetime
+        map["hens"] = monitoringCompanySituationData.hens
+        map["l_eggs"] = monitoringCompanySituationData.lEggs
+        map["m_eggs"] = monitoringCompanySituationData.mEggs
+        map["s_eggs"] = monitoringCompanySituationData.sEggs
+        map["situation_datetime"] = monitoringCompanySituationData.situationDatetime
+        map["l_eggs"] = monitoringCompanySituationData.lEggs
 
         return map
     }
