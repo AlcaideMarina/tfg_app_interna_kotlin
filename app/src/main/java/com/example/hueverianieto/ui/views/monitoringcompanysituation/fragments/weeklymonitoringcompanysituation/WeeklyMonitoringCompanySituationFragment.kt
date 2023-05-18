97c475a6e65daf7b922c1fd8a8338d6dd9365d50
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-18 18:29:18 +0200

    feat: Create weekly frg

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/monitoringcompanysituation/fragments/weeklymonitoringcompanysituation/WeeklyMonitoringCompanySituationFragment.kt
@@ -0,0 +1,46 @@
+package com.example.hueverianieto.ui.views.monitoringcompanysituation.fragments.weeklymonitoringcompanysituation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.databinding.FragmentWeeklyMonitoringCompanySituationBinding
+
+class WeeklyMonitoringCompanySituationFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentWeeklyMonitoringCompanySituationBinding
+    private lateinit var currentUserData: InternalUserData
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (activity as BaseActivity).configNav(true)
+
+        this.binding = FragmentWeeklyMonitoringCompanySituationBinding.inflate(
+            inflater, container, false
+        )
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        TODO("Not yet implemented")
+    }
+}
