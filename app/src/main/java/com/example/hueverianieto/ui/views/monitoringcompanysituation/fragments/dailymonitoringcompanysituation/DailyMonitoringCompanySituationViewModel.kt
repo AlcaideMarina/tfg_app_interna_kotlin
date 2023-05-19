6f5fc11eac0d64782f63fdfee29fefd829090727
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-19 03:10:58 +0200

    feat: Create VM with get fun

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/monitoringcompanysituation/fragments/dailymonitoringcompanysituation/DailyMonitoringCompanySituationViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.hueverianieto.ui.views.monitoringcompanysituation.fragments.dailymonitoringcompanysituation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hueverianieto.data.models.local.WeeklyMonitoringCompanySituationData
+import com.example.hueverianieto.data.models.remote.MonitoringCompanySituationData
+import com.example.hueverianieto.domain.usecases.GetDailyMonitoringCompanySituationUseCase
+import com.example.hueverianieto.ui.views.monitoringcompanysituation.fragments.weeklymonitoringcompanysituation.WeeklyMonitoringCompanySituationViewState
+import com.google.firebase.Timestamp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DailyMonitoringCompanySituationViewModel @Inject constructor(
+    val getDailyMonitoringCompanySituationUseCase: GetDailyMonitoringCompanySituationUseCase
+) : ViewModel() {
+
+    private val _viewState = MutableStateFlow(DailyMonitoringCompanySituationViewState())
+    val viewState: StateFlow<DailyMonitoringCompanySituationViewState> get() = _viewState
+
+    private val _monitoringCompanySituationData = MutableLiveData<MonitoringCompanySituationData?>()
+    val monitoringCompanySituationData: LiveData<MonitoringCompanySituationData?> get() = _monitoringCompanySituationData
+
+    fun getDailyMonitoringCompanySituation(timestamp: Timestamp) {
+        viewModelScope.launch {
+            _viewState.value = DailyMonitoringCompanySituationViewState(isLoading = true)
+            _monitoringCompanySituationData.value = getDailyMonitoringCompanySituationUseCase(timestamp)
+            _viewState.value = DailyMonitoringCompanySituationViewState(isLoading = false)
+        }
+    }
+
+}
