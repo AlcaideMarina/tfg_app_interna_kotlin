06b6b10119b8a0967fb6516271f57aa33703083d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-19 23:18:23 +0200

    feat: Create modify kt

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/monitoringcompanysituation/fragments/modifydailymonitoringcompanysituation/ModifyDailyMonitoringCompanySituationFragment.kt
@@ -0,0 +1,48 @@
+package com.example.hueverianieto.ui.views.monitoringcompanysituation.fragments.modifydailymonitoringcompanysituation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hueverianieto.base.BaseActivity
+import com.example.hueverianieto.base.BaseFragment
+import com.example.hueverianieto.base.BaseState
+import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.data.models.remote.MonitoringCompanySituationData
+import com.example.hueverianieto.databinding.FragmentDailyMonitoringCompanySituationBinding
+
+class ModifyDailyMonitoringCompanySituationFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentDailyMonitoringCompanySituationBinding
+    private lateinit var currentUserData: InternalUserData
+    private lateinit var monitoringCompanySituationData: MonitoringCompanySituationData
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        (activity as BaseActivity).configNav(true)
+
+        this.binding = FragmentDailyMonitoringCompanySituationBinding.inflate(
+            inflater, container, false
+        )
+        return this.binding.root
+    }
+
+    override fun configureUI() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setObservers() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUI(state: BaseState) {
+        TODO("Not yet implemented")
+    }
+}
