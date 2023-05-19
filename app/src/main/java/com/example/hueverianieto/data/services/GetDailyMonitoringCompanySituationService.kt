c70b735c4fab0fc6b8e71659cd2b26cea63aa211
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-19 03:09:21 +0200

    fix: Fix timestamp time

--- app/src/main/java/com/example/hueverianieto/data/services/GetDailyMonitoringCompanySituationService.kt
+++ app/src/main/java/com/example/hueverianieto/data/services/GetDailyMonitoringCompanySituationService.kt
@@ -1,5 +1,6 @@
 package com.example.hueverianieto.data.services
 
+import com.example.hueverianieto.utils.Utils
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.coroutines.tasks.await
@@ -12,7 +13,8 @@ class GetDailyMonitoringCompanySituationService @Inject constructor(
     suspend fun getDailyMonitoringCompanySituation(timestamp: Timestamp) : Result<QuerySnapshot> = runCatching {
         firebaseClient.db
             .collection("farm_situation")
-            .whereEqualTo("situation_datetime", timestamp)
+            .whereGreaterThanOrEqualTo("situation_datetime", timestamp)
+            .whereLessThan("situation_datetime", Timestamp(Utils.addToDate(timestamp.toDate(), 1)))
             .get()
             .await()
     }
