cb46b568e708401461ab0284d01efdc41e12779d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-19 03:10:21 +0200

    feat: Create VM with get function and observer

--- app/src/main/java/com/example/hueverianieto/ui/views/monitoringcompanysituation/fragments/dailymonitoringcompanysituation/DailyMonitoringCompanySituationFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/monitoringcompanysituation/fragments/dailymonitoringcompanysituation/DailyMonitoringCompanySituationFragment.kt
@@ -4,21 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
 import com.example.hueverianieto.data.models.remote.InternalUserData
+import com.example.hueverianieto.data.models.remote.MonitoringCompanySituationData
 import com.example.hueverianieto.databinding.FragmentDailyFinalProductControlBinding
 import com.example.hueverianieto.databinding.FragmentDailyMonitoringCompanySituationBinding
 import com.example.hueverianieto.utils.Utils
 import com.google.firebase.Timestamp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DailyMonitoringCompanySituationFragment : BaseFragment() {
 
     private lateinit var binding: FragmentDailyMonitoringCompanySituationBinding
     private lateinit var currentUserData: InternalUserData
     private lateinit var situationDatetime: Timestamp
+    private val dailyMonitoringCompanySituationViewModel: DailyMonitoringCompanySituationViewModel by viewModels()
+
+    private var monitoringCompanySituationData : MonitoringCompanySituationData? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,12 +48,16 @@ class DailyMonitoringCompanySituationFragment : BaseFragment() {
         // TODO: Llamada para conseguir el doc
             // TODO: Si el doc no existe -> seteamos todo a 0
             // TODO: Si el doc existe -> seteamos los valores
+        this.dailyMonitoringCompanySituationViewModel.getDailyMonitoringCompanySituation(situationDatetime)
         setDate()
         setButtons()
     }
 
     override fun setObservers() {
-        //TODO("Not yet implemented")
+        this.dailyMonitoringCompanySituationViewModel.monitoringCompanySituationData.observe(this) { data ->
+            monitoringCompanySituationData = data
+            setFields()
+        }
     }
 
     override fun setListeners() {
