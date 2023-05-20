bfa10b5c0e659de3755ba202e96e3de81fdae4c9
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-20 18:34:01 +0200

    feat: Create add UC

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/NewMonitoringCompanySituationUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.models.remote.MonitoringCompanySituationData
+import com.example.hueverianieto.data.services.NewDocumentService
+import com.example.hueverianieto.utils.FarmUtils
+import javax.inject.Inject
+
+class NewMonitoringCompanySituationUseCase @Inject constructor(
+    val newDocumentService: NewDocumentService
+) {
+
+    suspend operator fun invoke(monitoringCompanySituationData: MonitoringCompanySituationData) {
+        val dataMap = FarmUtils.monitoringCompanySituationParcelableToMap(monitoringCompanySituationData)
+        return when(newDocumentService.newDocument("farm_situation", dataMap).getOrNull()) {
+            null -> false
+            else -> true
+        }
+    }
+
+}
