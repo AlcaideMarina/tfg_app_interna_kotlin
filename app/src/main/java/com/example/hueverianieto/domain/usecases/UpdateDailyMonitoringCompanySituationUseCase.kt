c475997ed4d5b8ef1b56532dac308af6199b971b
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-20 12:48:22 +0200

    feat: Create update usecase

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/UpdateDailyMonitoringCompanySituationUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.models.remote.MonitoringCompanySituationData
+import com.example.hueverianieto.data.services.UpdateFirestoreService
+import javax.inject.Inject
+
+class UpdateDailyMonitoringCompanySituationUseCase @Inject constructor(
+    private val updateFirestoreService: UpdateFirestoreService
+) {
+
+    suspend operator fun invoke(dataMap: Map<String, Any?>, documentId: String) : Boolean =
+        updateFirestoreService.updateDocumentFirestore(dataMap, documentId, "farm_situation")
+
+}
