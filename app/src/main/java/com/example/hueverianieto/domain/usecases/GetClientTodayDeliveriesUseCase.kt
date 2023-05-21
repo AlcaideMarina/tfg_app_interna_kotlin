a7a9d70d1b2ade2449d5030a6cbb40a1c951bdf6
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-21 03:40:36 +0200

    feat: Create getDeliveriesUseCase

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/GetClientTodayDeliveriesUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.models.remote.OrderData
+import com.example.hueverianieto.data.services.GetClientTodayDeliveriesService
+import com.example.hueverianieto.data.services.GetClientTodayOrdersService
+import javax.inject.Inject
+
+class GetClientTodayDeliveriesUseCase @Inject constructor(
+    private val getClientTodayDeliveriesService: GetClientTodayDeliveriesService
+) {
+
+    suspend operator fun invoke(documentId : String) :List<OrderData?>? =
+        getClientTodayDeliveriesService.getClientTodayDeliveries(documentId)
+
+}
