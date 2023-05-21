a521333951b44d1d6e4f8efe64befca066606881
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-21 02:42:56 +0200

    feat: Create ViewState

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/home/HomeViewState.kt
@@ -0,0 +1,8 @@
+package com.example.hueverianieto.ui.views.main.fragments.home
+
+import com.example.hueverianieto.base.BaseState
+
+class HomeViewState (
+    var isLoading: Boolean = false,
+    var error: Boolean = false
+) : BaseState
