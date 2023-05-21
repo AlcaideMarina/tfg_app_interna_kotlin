ecea212f12acdd879bdfc855a3a21e76a3d8399d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-21 02:45:07 +0200

    feat: Add VM in frg

--- app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/home/HomeFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.hueverianieto.R
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
 import com.example.hueverianieto.data.models.remote.InternalUserData
 import com.example.hueverianieto.databinding.FragmentHomeBinding
 import com.example.hueverianieto.ui.views.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var currentUserData: InternalUserData
+
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun configureUI() {
         //TODO("Not yet implemented")
