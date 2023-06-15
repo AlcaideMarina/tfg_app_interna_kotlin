7df6c6b7136eddf63b0a9f1ccad45293ebe4dee8
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-16 00:53:34 +0200

    feat: Create change pass use case

---
+++ app/src/main/java/com/example/hueverianieto/domain/usecases/ChangePasswordUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.hueverianieto.domain.usecases
+
+import com.example.hueverianieto.data.services.ChangePasswordService
+import javax.inject.Inject
+
+class ChangePasswordUseCase @Inject constructor(
+    private val changePasswordService: ChangePasswordService
+) {
+
+    suspend operator fun invoke(oldPass: String, newPass: String) : Boolean {
+        val result = changePasswordService.changePassword(oldPass, newPass)
+        return result.isSuccess
+    }
+
+}
