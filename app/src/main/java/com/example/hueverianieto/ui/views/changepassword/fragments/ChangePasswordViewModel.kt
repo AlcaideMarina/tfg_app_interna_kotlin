796ecf570a283e002196e77e351986a4c2dfddfd
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-16 00:53:58 +0200

    feat: Create change pass viewModel

---
+++ app/src/main/java/com/example/hueverianieto/ui/views/changepassword/fragments/ChangePasswordViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.hueverianieto.ui.views.changepassword.fragments
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hueverianieto.data.models.local.AlertOkData
+import com.example.hueverianieto.domain.usecases.ChangePasswordUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ChangePasswordViewModel @Inject constructor(
+    val changePasswordUseCase: ChangePasswordUseCase
+) : ViewModel() {
+
+    private var _viewState = MutableStateFlow(ChangePasswordViewState())
+    val viewState: StateFlow<ChangePasswordViewState> get() = _viewState
+
+    private var _alertDialog = MutableLiveData(AlertOkData())
+    val alertDialog: LiveData<AlertOkData> get() = _alertDialog
+
+    fun checkOldPassword(oldPass: String, newPass: String) {
+        viewModelScope.launch {
+            _viewState.value = ChangePasswordViewState(isLoading = true)
+            when(val result = changePasswordUseCase(oldPass, newPass)) {
+                false -> {
+                    _alertDialog.value = AlertOkData(
+                        "Error",
+                        "Se ha producido un error cuando se estaban actualizado los datos. Por favor, revise los datos e inténtelo de nuevo.",
+                        true
+                    )
+                }
+                true -> {
+                    _alertDialog.value = AlertOkData(
+                        "Contraseña actualizada",
+                        "La contraseña se ha actualizado correctamente. A partir de ahora, cuando inicie sesión, hágalo con esta contraseña.",
+                        true,
+                        customCode = 0
+                    )
+                }
+            }
+            _viewState.value = ChangePasswordViewState(isLoading = false)
+        }
+    }
+
+}
