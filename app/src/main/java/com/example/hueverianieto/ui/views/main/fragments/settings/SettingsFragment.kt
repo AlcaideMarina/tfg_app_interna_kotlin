2f60347a2fb6cba4da5014ea22b0927a95f9816d
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-22 03:22:31 +0200

    refactor: Set title in settings frg

--- app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/hueverianieto/ui/views/main/fragments/settings/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.example.hueverianieto.base.BaseActivity
 import com.example.hueverianieto.base.BaseFragment
 import com.example.hueverianieto.base.BaseState
 import com.example.hueverianieto.data.models.remote.InternalUserData
@@ -28,6 +29,7 @@ class SettingsFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         (activity as MainActivity).configNav(false)
+        (activity as BaseActivity).changeTopBarName("Ajustes")
         this.binding = FragmentSettingsBinding.inflate(
             inflater, container, false
         )
